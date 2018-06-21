631e089d845d925fb5d50cd5d5068d870aba5f95
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-06-21 18:09:20 -0300

    Improve a11y

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>Welcome!</h1>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vue.js_Logo.svg/2000px-Vue.js_Logo.svg.png">
+    <img alt="vue" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Vue.js_Logo.svg/2000px-Vue.js_Logo.svg.png">
   </div>
 </template>
 
